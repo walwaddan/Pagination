f739c3f152c7976d482f6409108c8c7bb62da7ee
Mr. Tester <35148050+walwaddan@users.noreply.github.com>
2022-01-31 17:55:47 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import Posts from './comp/Posts'
+import Pagingin from './comp/Pagingin'
+import axios from 'axios';
+import './App.css';
+
+const Appp = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  return (
+    <div className=''>
+      <h1 className=''>My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagingin
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+    </div>
+  );
+};
+
+export default Appp;
